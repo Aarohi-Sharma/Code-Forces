6d14ca01f16e4c825be4f3859eb8b7c26b40d614
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2025-03-22 13:34:09 +0530

    1915A- Odd One Out.cpp

---
+++ 1915A- Odd One Out.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<vector<int>> arr(n, vector<int>(3));
+    for(int i=0; i<n; i++){
+        cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
+    }
+    for(auto &i: arr){
+        sort(i.begin(), i.end());
+    }
+    vector<int> out(n);
+    for(int i=0; i<n; i++){
+        if(arr[i][0]==arr[i][1]) out[i]=arr[i][2];
+        else out[i]=arr[i][0];
+    }
+    for(int i=0; i<n; i++){
+        cout<<out[i]<<endl;
+    }
+}
